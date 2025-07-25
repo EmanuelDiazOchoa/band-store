ad51f7398b028030c4adfa8f9290f0557fe6b5c7
EmanuelDiazOchoa <emagnr93@gmail.com>
2025-07-25 00:01:25 -0300

    Feat: Gallery mejorado

--- src/components/Gallery.jsx
+++
@@ -1,8 +0,0 @@
-export default function Gallery() {
-  return (
-    <section className="p-8 text-white">
-      <h2 className="text-3xl font-bold mb-4">Galería</h2>
-      <p>Contenido de la galería aquí.</p>
-    </section>
-  );
-}
--- src/components/MySwiper.jsx
+++ src/components/MySwiper.jsx
@@ -1,159 +1,139 @@
 import "./MySwiper.css";
 import "./swiper-gl.css";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { A11y, Autoplay, Navigation } from "swiper/modules";
-import SwiperGL from "./swiper-gl.esm.js";
+
 import "swiper/css";
 import "swiper/css/a11y";
 import "swiper/css/autoplay";
 import "swiper/css/navigation";
-import "./swiper-gl.css";
-import "./MySwiper.css";
 
 export default function MySwiper() {
+  const [swiperModules, setSwiperModules] = useState([A11y, Autoplay, Navigation]);
+
+
+  useEffect(() => {
+    import("../lib/swiper-gl.esm.js").then((module) => {
+      setSwiperModules((mods) => [...mods, module.default]);
+    });
+  }, []);
+
   const swiperParameters = {
-    modules: [A11y, Autoplay, Navigation, SwiperGL],
+    modules: swiperModules,
     effect: "gl",
     speed: 1500,
     navigation: { enabled: true },
     autoplay: { enabled: true, delay: 500 },
-    breakpoints: { 768: {}, 1280: {}, 1920: {} },
+    breakpoints: {
+      768: {},
+      1280: {},
+      1920: {},
+    },
   };
+
   return (
-    <>
-      <Swiper {...swiperParameters}>
-        <SwiperSlide className="swiper-slide-5215">
-          <img
-            className="swiper-slide-bg-image swiper-slide-bg-image-bdb6 swiper-gl-image"
-            src="../../public/img/1Floyd.jpg"
-          />
+    <Swiper {...swiperParameters}>
+      <SwiperSlide className="swiper-slide-5215">
+        <img
+          className="swiper-slide-bg-image swiper-gl-image"
+          src="/img/1Floyd.jpg"
+        />
+        <div className="swiper-slide-content">
+          <div className="swiper-slide-text">Pink Floyd</div>
+        </div>
+      </SwiperSlide>
 
-          <div className="swiper-slide-content swiper-slide-content-94be">
-            <div className="swiper-slide-text swiper-slide-text-66a3">
-              Pink Floyd 
-            </div>
-          </div>
-        </SwiperSlide>
+      <SwiperSlide className="swiper-slide-5215">
+        <img
+          className="swiper-slide-bg-image swiper-gl-image"
+          src="/img/2Roses.webp"
+        />
+        <div className="swiper-slide-content">
+          <div className="swiper-slide-text">Guns n Roses</div>
+        </div>
+      </SwiperSlide>
 
-        <SwiperSlide className="swiper-slide-5215">
-          <img
-            className="swiper-slide-bg-image swiper-slide-bg-image-bdb6 swiper-gl-image"
-            src="../../public/img/2Roses.webp"
-          />
+      <SwiperSlide className="swiper-slide-5215">
+        <img
+          className="swiper-slide-bg-image swiper-gl-image"
+          src="/img/3Peppers.png"
+        />
+        <div className="swiper-slide-content">
+          <div className="swiper-slide-text">Red Hot Chilli Peppers</div>
+        </div>
+      </SwiperSlide>
 
-          <div className="swiper-slide-content swiper-slide-content-94be">
-            <div className="swiper-slide-text swiper-slide-text-66a3">
-              Guns n Roses
-            </div>
-          </div>
-        </SwiperSlide>
+      <SwiperSlide className="swiper-slide-5215">
+        <img
+          className="swiper-slide-bg-image swiper-gl-image"
+          src="/img/4Sabbath.jpg"
+        />
+        <div className="swiper-slide-content">
+          <div className="swiper-slide-text">Black Sabbath</div>
+        </div>
+      </SwiperSlide>
 
-        <SwiperSlide className="swiper-slide-5215">
-          <img
-            className="swiper-slide-bg-image swiper-slide-bg-image-bdb6 swiper-gl-image"
-            src="https://studio.swiperjs.com/demo-images/movies/03.jpg"
-          />
+      <SwiperSlide className="swiper-slide-5215">
+        <img
+          className="swiper-slide-bg-image swiper-gl-image"
+          src="/img/5Beatles.jpg"
+        />
+        <div className="swiper-slide-content">
+          <div className="swiper-slide-text">The Beatles</div>
+        </div>
+      </SwiperSlide>
 
-          <div className="swiper-slide-content swiper-slide-content-94be">
-            <div className="swiper-slide-text swiper-slide-text-66a3">
-              Red Hot Chilli Peppers
-            </div>
-          </div>
-        </SwiperSlide>
+      <SwiperSlide className="swiper-slide-5215">
+        <img
+          className="swiper-slide-bg-image swiper-gl-image"
+          src="/img/6Zeppelin.jpg"
+        />
+        <div className="swiper-slide-content">
+          <div className="swiper-slide-text">Led Zeppelin</div>
+        </div>
+      </SwiperSlide>
 
-        <SwiperSlide className="swiper-slide-5215">
-          <img
-            className="swiper-slide-bg-image swiper-slide-bg-image-bdb6 swiper-gl-image"
-            src="https://studio.swiperjs.com/demo-images/movies/04.jpg"
-          />
+      <SwiperSlide className="swiper-slide-5215">
+        <img
+          className="swiper-slide-bg-image swiper-gl-image"
+          src="/img/7Rem.jpeg"
+        />
+        <div className="swiper-slide-content">
+          <div className="swiper-slide-text">R.E.M</div>
+        </div>
+      </SwiperSlide>
 
-          <div className="swiper-slide-content swiper-slide-content-94be">
-            <div className="swiper-slide-text swiper-slide-text-66a3">
-              Black Sabbath 
-            </div>
-          </div>
-        </SwiperSlide>
+      <SwiperSlide className="swiper-slide-5215">
+        <img
+          className="swiper-slide-bg-image swiper-gl-image"
+          src="/img/8Acdc.webp"
+        />
+        <div className="swiper-slide-content">
+          <div className="swiper-slide-text">AC/DC</div>
+        </div>
+      </SwiperSlide>
 
-        <SwiperSlide className="swiper-slide-5215">
-          <img
-            className="swiper-slide-bg-image swiper-slide-bg-image-bdb6 swiper-gl-image"
-            src="https://studio.swiperjs.com/demo-images/movies/05.jpg"
-          />
+      <SwiperSlide className="swiper-slide-5215">
+        <img
+          className="swiper-slide-bg-image swiper-gl-image"
+          src="/img/9Rammstein.jpg"
+        />
+        <div className="swiper-slide-content">
+          <div className="swiper-slide-text">Rammstein</div>
+        </div>
+      </SwiperSlide>
 
-          <div className="swiper-slide-content swiper-slide-content-94be">
-            <div className="swiper-slide-text swiper-slide-text-66a3">
-              The Beatles
-            </div>
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide className="swiper-slide-5215">
-          <img
-            className="swiper-slide-bg-image swiper-slide-bg-image-bdb6 swiper-gl-image"
-            src="https://studio.swiperjs.com/demo-images/movies/06.jpg"
-          />
-
-          <div className="swiper-slide-content swiper-slide-content-94be">
-            <div className="swiper-slide-text swiper-slide-text-66a3">
-              Led Zeppelin
-            </div>
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide className="swiper-slide-5215">
-          <img
-            className="swiper-slide-bg-image swiper-slide-bg-image-bdb6 swiper-gl-image"
-            src="https://studio.swiperjs.com/demo-images/movies/07.jpg"
-          />
-
-          <div className="swiper-slide-content swiper-slide-content-94be">
-            <div className="swiper-slide-text swiper-slide-text-66a3">
-              R.E.M
-            </div>
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide className="swiper-slide-5215">
-          <img
-            className="swiper-slide-bg-image swiper-slide-bg-image-bdb6 swiper-gl-image"
-            src="https://studio.swiperjs.com/demo-images/movies/08.jpg"
-          />
-
-          <div className="swiper-slide-content swiper-slide-content-94be">
-            <div className="swiper-slide-text swiper-slide-text-66a3">
-              AC/DC
-            </div>
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide className="swiper-slide-5215">
-          <img
-            className="swiper-slide-bg-image swiper-slide-bg-image-bdb6 swiper-gl-image"
-            src="https://studio.swiperjs.com/demo-images/movies/09.jpg"
-          />
-
-          <div className="swiper-slide-content swiper-slide-content-94be">
-            <div className="swiper-slide-text swiper-slide-text-66a3">
-              Rammstein
-            </div>
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide className="swiper-slide-5215">
-          <img
-            className="swiper-slide-bg-image swiper-slide-bg-image-bdb6 swiper-gl-image"
-            src="https://studio.swiperjs.com/demo-images/movies/10.jpg"
-          />
-
-          <div className="swiper-slide-content swiper-slide-content-94be">
-            <div className="swiper-slide-text swiper-slide-text-66a3">
-              Rolling Stone
-            </div>
-          </div>
-        </SwiperSlide>
-      </Swiper>
-    </>
+      <SwiperSlide className="swiper-slide-5215">
+        <img
+          className="swiper-slide-bg-image swiper-gl-image"
+          src="/img/10Stone.jpg"
+        />
+        <div className="swiper-slide-content">
+          <div className="swiper-slide-text">Rolling Stone</div>
+        </div>
+      </SwiperSlide>
+    </Swiper>
   );
 }
